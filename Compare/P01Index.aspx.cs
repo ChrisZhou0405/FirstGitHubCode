e74f9f083f3ce2c1b4c262e66787a9812b350cc3
Chris <30383720+ChrisZhou0405@users.noreply.github.com>
2017-07-23 19:13:21 +0800

    Add function Online

--- Compare/P01Index.aspx.cs
+++ Compare/P01Index.aspx.cs
@@ -18,12 +18,17 @@ namespace Compare
         {
             if (code == "0102")
             {
-                return "You";
+                return "You"+"WHYME";
             }
             if(code=="002")
             {return "ME";}
             return "Chris Lee"+code;
+            
 
+        }
+        pubic void SayHi()
+        {
+           
         }
     }
 }
